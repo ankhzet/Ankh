78528d83b1f8da4fa7cb69eab1a117c7495e14c0
AnkhZet <ankhzet@gmail.com>
2015-06-25 09:40:51 +0300

    More versatile authors table seeder

--- database/seeds/AuthorsTableSeeder.php
+++ database/seeds/AuthorsTableSeeder.php
@@ -5,13 +5,16 @@
 	use Ankh\Author;
 
 	class AuthorsTableSeeder extends Seeder {
+		const AUTHORS_TO_SEED = 200;
+		var $fakers = null;
 
 		public function run() {
 
-			$faker = Faker\Factory::create();
-
-			for ($i = 0; $i < $faker->numberBetween(1, 100); $i++)
-				$this->createAuthor($faker->name(), '/' . str_replace('-', '_', $faker->slug()));
+			$faker = $this->faker();
+			for ($i = 0; $i < $faker->numberBetween(1, self::AUTHORS_TO_SEED); $i++) {
+				$faker = $this->faker();
+				$this->createAuthor($faker->boolean(30) ? mb_strtolower($faker->name()) : $faker->name(), '/' . str_replace('-', '_', $faker->slug()));
+			}
 
 		}
 
@@ -21,6 +24,17 @@
 			$author->link = $link;
 			$author->save();
 			return $author;
+		}
+
+		function faker() {
+			if (!$this->fakers) {
+				foreach (['ru_RU', 'en_US', 'de_DE'] as $locale)
+					$this->fakers[] = Faker\Factory::create($locale);
+
+				$this->fakers = Illuminate\Support\Collection::make($this->fakers);
+			}
+
+			return $this->fakers->random();
 		}
 
 	}

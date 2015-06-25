2ef40c20caded73949e79c432b6e59da895bc9d8
AnkhZet <ankhzet@gmail.com>
2015-06-25 14:38:22 +0300

    Added softdeletes to authors table

--- database/migrations/2015_06_24_111038_create_authors_table.php
+++ database/migrations/2015_06_24_111038_create_authors_table.php
@@ -1,32 +1,30 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
+	use Illuminate\Database\Schema\Blueprint;
+	use Illuminate\Database\Migrations\Migration;
 
-class CreateAuthorsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('authors', function(Blueprint $table) {
-            $table->increments('id');
-            $table->string('fio', 250)->unique();
-            $table->string('link', 250)->unique();
-            $table->timestamps();
-        });
-    }
+	class CreateAuthorsTable extends Migration {
+		/**
+		 * Run the migrations.
+		 *
+		 * @return void
+		 */
+		public function up() {
+			Schema::create('authors', function(Blueprint $table) {
+				$table->increments('id');
+				$table->string('fio', 250)->unique();
+				$table->string('link', 250)->unique();
+				$table->timestamps();
+				$table->softDeletes();
+			});
+		}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop('authors');
-    }
-}
+		/**
+		 * Reverse the migrations.
+		 *
+		 * @return void
+		 */
+		public function down() {
+			Schema::drop('authors');
+		}
+	}

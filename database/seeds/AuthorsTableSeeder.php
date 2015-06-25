24b87d4932290446c6c951cd782625e5a8a5ca65
AnkhZet <ankhzet@gmail.com>
2015-06-25 14:35:49 +0300

    Updated AuthorsSeeder to use helper class

--- database/seeds/AuthorsTableSeeder.php
+++ database/seeds/AuthorsTableSeeder.php
@@ -4,17 +4,15 @@
 
 	use Ankh\Author;
 
-	class AuthorsTableSeeder extends Seeder {
+	class AuthorsTableSeeder extends CommonSeeder {
+		protected $locales = ['ru_RU', 'en_US', 'de_DE'];
 		const AUTHORS_TO_SEED = 200;
-		var $fakers = null;
 
 		public function run() {
-
-			$faker = $this->faker();
-			for ($i = 0; $i < $faker->numberBetween(1, self::AUTHORS_TO_SEED); $i++) {
+			$this->iterate(self::AUTHORS_TO_SEED / 2, self::AUTHORS_TO_SEED, function () {
 				$faker = $this->faker();
 				$this->createAuthor($faker->boolean(30) ? mb_strtolower($faker->name()) : $faker->name(), '/' . str_replace('-', '_', $faker->slug()));
-			}
+			});
 
 		}
 
@@ -24,17 +22,6 @@
 			$author->link = $link;
 			$author->save();
 			return $author;
-		}
-
-		function faker() {
-			if (!$this->fakers) {
-				foreach (['ru_RU', 'en_US', 'de_DE'] as $locale)
-					$this->fakers[] = Faker\Factory::create($locale);
-
-				$this->fakers = Illuminate\Support\Collection::make($this->fakers);
-			}
-
-			return $this->fakers->random();
 		}
 
 	}

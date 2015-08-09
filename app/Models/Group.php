c2f873ae76092d33f478f94153a151c31b00d3b2
AnkhZet <ankhzet@gmail.com>
2015-08-09 21:01:07 +0300

    Implement updateable functionality
    Implement updateable functionality for different entity classes

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -3,7 +3,7 @@
 	use Ankh\Author;
 	use Ankh\Page;
 
-	class Group extends Entity {
+	class Group extends Updateable {
 
 		protected $guarded = ['id'];
 
@@ -19,6 +19,14 @@
 			$paginator = $this->pages()->take($amount)->orderBy('updated_at', 'desc');
 			$delta = $paginator->count() - $amount;
 			return $paginator->get();
+		}
+
+		protected function updateType() {
+			return GroupUpdate::TYPE;
+		}
+
+		protected function updateClass() {
+			return GroupUpdate::class;
 		}
 
 	}

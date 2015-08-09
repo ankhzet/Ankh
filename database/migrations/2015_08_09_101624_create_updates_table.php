4415d4ee8c51e1d18465d30dfc2200301630ea1a
AnkhZet <ankhzet@gmail.com>
2015-08-09 20:51:11 +0300

    Add migrations for required tables

---
+++ database/migrations/2015_08_09_101624_create_updates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUpdatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('updates', function(Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('type');
+            $table->integer('value');
+            $table->integer('delta');
+            $table->string('change');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('updates');
+    }
+}

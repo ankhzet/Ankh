54c354980c5c1c1c3b3f99bd32a4380595483ab7
AnkhZet <ankhzet@gmail.com>
2015-08-13 00:18:24 +0300

    Implement Feed class on top of Roumen\Feed

--- app/Models/Feeds/Feed.php
+++ app/Models/Feeds/Feed.php
@@ -10,10 +10,27 @@ use Roumen\Feed\Facades\Feed as FeedEngine;
 
 class Feed implements FeedContract {
 
+	protected $feeder;
+
 	public function __construct(FeedChanel $chanel = null) {
+		$this->feeder = FeedEngine::make();
+		$this->feeder->lang = Config::get('app.locale', 'en');
+		$this->feeder->logo = asset('assets/img/logo.png');
+		$this->feeder->setDateFormat('carbon');
+		$this->feeder->setShortening(false);
+		$this->feeder->description = Lang::get('pages.rss.description');
 	}
 
 	public function make(FeedChanelContract $chanel) {
+		$this->feeder->title = Lang::get('common.site') . ' - ' . $chanel->title();
+
+		$this->feeder->link = $chanel->url();
+
+		$this->feeder->pubdate = $chanel->feedItems(function ($item) {
+			$this->feeder->add($item->title, $item->author, $item->url, $item->created, $item->description, $item->content);
+		});
+
+		return $this;
 	}
 
 
@@ -22,6 +39,7 @@ class Feed implements FeedContract {
 	}
 
 	public function render() {
+		return $this->feeder->render($this->format());
 	}
 
 }

81b065981dbe4ec31981fe3e822a8ab3e4ba589a
AnkhZet <ankhzet@gmail.com>
2015-09-02 18:05:51 +0300

    Add C/U/D handlers for Updateable

--- app/Models/Updateable.php
+++ app/Models/Updateable.php
@@ -1,8 +1,64 @@
 <?php namespace Ankh;
 
+use Closure;
+
 use Ankh\Update;
 
 class Updateable extends Entity {
+
+	const DELETED_AT = 'deleted_at';
+
+	protected static function boot() {
+		parent::boot();
+
+		static::attachEventListeners();
+	}
+
+	protected static function attachEventListeners() {
+		static::created(function ($updateable) {
+			$updateable->wasCreated();
+		});
+
+		static::updating(function($updateable) {
+			if ($diff = $updateable->diffAttributes())
+				$updateable->willBeUpdated($diff);
+		});
+
+		static::deleting(function ($updateable) {
+			$updateable->willBeDeleted();
+		});
+	}
+
+	protected function diffAttributes() {
+		$timestamps = [static::CREATED_AT, static::UPDATED_AT, static::DELETED_AT];
+		$original = array_except($this->getOriginal(), $timestamps);
+		$dirty    = array_except($this->getAttributes(), $timestamps);
+
+		return array_diff($dirty, $original);
+	}
+
+	protected function wasCreated(Closure $callback = null) {
+	}
+
+	protected function willBeUpdated($dirty) {
+	}
+
+	protected function willBeDeleted(Closure $callback = null) {
+	}
+
+	public function newUpdate($type, Closure $callback = null) {
+		$class = $this->updateClass();
+		$update = new $class;
+
+		$update->type = $type;
+
+		if ($callback)
+			$callback($update);
+
+		$update->save();
+
+		$this->attachUpdate($update);
+	}
 
 	public function updates() {
 		return $this->belongsToMany($this->updateClass(), 'entity_update', 'entity_id', 'update_id')

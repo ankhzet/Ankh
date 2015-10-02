cf8728167910743bef2fe99c2d09726e129469ae
AnkhZet <ankhzet@gmail.com>
2015-10-02 07:14:00 +0300

    Verbose page checks

--- app/Jobs/CheckPage.php
+++ app/Jobs/CheckPage.php
@@ -2,6 +2,7 @@
 
 use Carbon\Carbon;
 use Queue;
+use Log;
 
 use Ankh\Page\Comparator;
 use Ankh\Version;
@@ -26,13 +27,14 @@ class CheckPage extends Job {
 		$page = $update->relatedPage();
 		$version = $page->version($update->created_at);
 
+		Log::info("Checking page " . $page->id . "...");
 		$compare = with(new Comparator)->compareLast($version);
 
 		if ($compare === false)
 			throw new \Exception("Page {$page->id} check failed");
 
 		if ($compare->equals()) {
-
+			Log::info("Page {$page->id} check found no differences");
 		} else {
 			$update->restore();
 		}

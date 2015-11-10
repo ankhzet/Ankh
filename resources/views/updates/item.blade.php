034c7158b1e85bb38df56da4944034fcd3cf022f
AnkhZet <ankhzet@gmail.com>
2015-11-10 23:58:04 +0200

    Make minor views fixes

--- resources/views/updates/item.blade.php
+++ resources/views/updates/item.blade.php
@@ -1,22 +1,24 @@
 
 					<div class="title update @if(!$last) dotted @endif ">
+						@admin()
+							@i-menu(icons normal-icons left)
+								@m-delete(!trash, route('updates.destroy', $update) )
+							@endmenu
+						@endadmin
+						<div style="overflow: hidden;">
 @if ($group = $update->relatedGroup())
-						<span class="head">
-							<a href="{{ route('groups.show', $group) }}">{{$group->title}}</a>@if ($update->relatedPage()):@endif
-						</span>
+							<span class="head">
+								<a href="{{ route('groups.show', $group) }}">{{$group->title}}</a>@if ($update->relatedPage()):@endif
+							</span>
 
 @endif
 @if ($page = $update->relatedPage())
-						<span class="head"><a href="{{ route('pages.show', $page) }}">{{$page->title}}</a></span>
+							<span class="head"><a href="{{ route('pages.show', $page) }}">{{$page->title}}</a></span>
 
 @endif
-						<div class="head small">
-							{!! $update_tag !!}
-							@admin()
-								@i-menu(right)
-									@m-delete('common.delete', route('updates.destroy', $update) )
-								@endmenu
-							@endadmin
-							<span class="link time">{{$update->created_at}}</span>
+							<div class="head small">
+								{!! $update_tag !!}
+								<span class="link time">{{$update->created_at}}</span>
+							</div>
 						</div>
 					</div>

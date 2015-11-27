dd3e19ac6422473f5fa275cda76b2cc723fc1fe7
AnkhZet <ankhzet@gmail.com>
2015-11-28 01:11:48 +0200

    Make minor view markup cleanup

--- resources/views/groups/item.blade.php
+++ resources/views/groups/item.blade.php
@@ -4,14 +4,15 @@
 							@kept(author)<a href="{{ route('authors.show', $group->author) }}">{{$group->author->fio}}</a> - @endkept <a href="{{ route('groups.show', $group) }}">{{$group->title}}</a>
 						</span>
 						@samlib($group)
+
 					</div>
 					<div class="text">
-						@if (trim($group->annotation) != '') {!! $group->annotation !!} <br/> @endif
+@if (trim($group->annotation) != '')						{!! $group->annotation !!} <br/> @endif
 @foreach ($group->pickPages(4)->allTrashed($delta) as $page)
-								&rarr; <a href="{{ route('pages.show', $page) }}" @if($page->trashed()) class="trashed" @endif>{{$page->title}}</a><br />
+						&rarr; <a href="{{ route('pages.show', $page) }}" @if($page->trashed()) class="trashed" @endif>{{$page->title}}</a><br />
 @endforeach
 @if ($delta > 0)
-								&rarr; <a href="{{ route('groups.pages.index', $group) }}">@lang('pages.pages.another', ['count' => $delta])</a><br />
+						&rarr; <a href="{{ route('groups.pages.index', $group) }}">@lang('pages.pages.another', ['count' => $delta])</a><br />
 @endif
 					</div>
 				</div>

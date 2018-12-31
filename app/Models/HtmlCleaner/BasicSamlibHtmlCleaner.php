767d8c1aa63779812c3020a780f254ace8c135ac
ankhzet <ankhzet@gmail.com>
2018-12-31 19:43:33 +0200

    add images preview

--- app/Models/HtmlCleaner/BasicSamlibHtmlCleaner.php
+++ app/Models/HtmlCleaner/BasicSamlibHtmlCleaner.php
@@ -6,6 +6,7 @@ class BasicSamlibHtmlCleaner extends BasicHtmlCleaner {
 
 	public function clean($html, $encoding = 'utf8') {
 		$html = $this->cleanHtml($html);
+		$html = $this->wrapExternalImages($html);
 		$html = $this->resolveImages($html);
 		return $html;
 	}
@@ -13,6 +14,15 @@ class BasicSamlibHtmlCleaner extends BasicHtmlCleaner {
 	public function resolveImages($html) {
 		return preg_replace_callback('|<img ([^>]*?)(src=(["\']?))/([^>\3]+\3[^>]*)>|i', function ($matches) {
 			return '<img ' . $matches[1] . $matches[2] . self::SAMLIB_DOMAIN . $matches[4] . ' \>';
+		}, $html);
+	}
+
+	public function wrapExternalImages($html) {
+		return preg_replace_callback('#(^|\s)(https?://.*?\.(jpe?g|png|gif|bmp))#iu', function ($matches) {
+			$leading = $matches[1];
+			$src = $matches[2];
+
+			return "$leading<a class=\"inline-preview\" href=\"$src\" target=\"_blank\"><img src=\"$src\" title=\"$src\" alt=\"$src\"/></a>";
 		}, $html);
 	}
 

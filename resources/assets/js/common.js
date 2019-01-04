7cf3ede1c671a987ef3007f75de9002038cf6fd5
ankhzet <ankhzet@gmail.com>
2019-01-04 09:18:08 +0200

    refactoring

--- resources/assets/js/common.js
+++ resources/assets/js/common.js
@@ -1,86 +1,84 @@
-$(function() {
-	$.ajaxSetup({
-		headers: {
-			'X-CSRF-Token': $('meta[name="_token"]').attr('content')
-		}
-	});
-
-	$('.log .stack').click(function () {
-		var expanded = 'expanded', has = $(this).hasClass(expanded);
-
-		$('.log .stack').animate({height: '50px', 'overflow': 'hidden'}, {duration:100})
-		.removeClass(expanded);
-
-		if (has)
-			return;
-
-		$(this).animate({height: '400px', 'overflow': 'auto'}, {duration:100}).addClass(expanded);
-	});
-});
-
 (function($) {
-
-	var laravel = {
-		initialize: function() {
-			this.methodLinks = $('a[data-method]');
-
-			this.registerEvents();
-		},
-
-		registerEvents: function() {
-			this.methodLinks.click(this.handleMethod);
-		},
-
-		handleMethod: function(e) {
-			var link = $(this);
-			var httpMethod = link.data('method').toUpperCase();
-			var form;
-
-			// If the data-method attribute is not PUT or DELETE,
-			// then we don't know what to do. Just ignore.
-			if ( $.inArray(httpMethod, ['PUT', 'DELETE']) === - 1 ) {
-				return;
+	$(function() {
+		$.ajaxSetup({
+			headers: {
+				'X-CSRF-Token': $('meta[name="_token"]').attr('content')
 			}
+		});
 
-			// Allow user to optionally provide data-no-confirm
-			if ( !!link.data('no-confirm') || !laravel.verifyConfirm(link) )
-				return false;
+		$('.log .stack').click(function () {
+			var expanded = 'expanded', has = $(this).hasClass(expanded);
 
-			form = laravel.createForm(link);
-			form.submit();
+			$('.log .stack').animate({ height: '50px', 'overflow': 'hidden' }, {duration:100})
+				.removeClass(expanded);
 
-			e.preventDefault();
-		},
+			if (has)
+				return;
 
-		verifyConfirm: function(link) {
-			return confirm(link.data('confirm') || "Are you sure?");
-		},
+			$(this).animate({ height: '400px', 'overflow': 'auto' }, {duration:100}).addClass(expanded);
+		});
 
-		createForm: function(link) {
-			var form =
-			$('<form>', {
-				'method': 'POST',
-				'action': link.attr('href')
-			});
+		var laravel = {
+			initialize: function() {
+				this.methodLinks = $('a[data-method]');
 
-			var hiddenInput =
-			$('<input>', {
-				'name': '_method',
-				'type': 'hidden',
-				'value': link.data('method').toUpperCase()
-			});
-			var token =
-			$('<input>', {
-				'name': '_token',
-				'type': 'hidden',
-				'value': $('meta[name="_token"]').attr('content')
-			});
+				this.registerEvents();
+			},
 
-			return form.append(token, hiddenInput)
-			.appendTo('body');
-		}
-	};
+			registerEvents: function() {
+				this.methodLinks.click(this.handleMethod);
+			},
 
-	laravel.initialize();
+			handleMethod: function(e) {
+				var link = $(this);
+				var httpMethod = link.data('method').toUpperCase();
+				var form;
 
+				// If the data-method attribute is not PUT or DELETE,
+				// then we don't know what to do. Just ignore.
+				if ( $.inArray(httpMethod, ['PUT', 'DELETE']) === - 1 ) {
+					return;
+				}
+
+				// Allow user to optionally provide data-no-confirm
+				if ( !!link.data('no-confirm') || !laravel.verifyConfirm(link) )
+					return false;
+
+				form = laravel.createForm(link);
+				form.submit();
+
+				e.preventDefault();
+			},
+
+			verifyConfirm: function(link) {
+				return confirm(link.data('confirm') || "Are you sure?");
+			},
+
+			createForm: function(link) {
+				var form =
+					$('<form>', {
+						'method': 'POST',
+						'action': link.attr('href')
+					});
+
+				var hiddenInput =
+					$('<input>', {
+						'name': '_method',
+						'type': 'hidden',
+						'value': link.data('method').toUpperCase()
+					});
+				var token =
+					$('<input>', {
+						'name': '_token',
+						'type': 'hidden',
+						'value': $('meta[name="_token"]').attr('content')
+					});
+
+				return form.append(token, hiddenInput)
+					.appendTo('body');
+			}
+		};
+
+		laravel.initialize();
+	});
 })(jQuery);
